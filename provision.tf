09b440b59cef77761dcac9b58acad2a2c82267a4
Arian van Putten <arian.vanputten@gmail.com>
2020-09-05 18:25:24 +0200

    Move providers to versions.tf

--- provision.tf
+++ provision.tf
@@ -1,19 +1,3 @@
-terraform {
-  required_providers {
-    packet = {
-      source  = "packethost/packet"
-      version = "3.0.1"
-    }
-    ct = {
-      source  = "poseidon/ct"
-      version = "~> 0.6.1"
-    }
-    digitalocean = {
-      source  = "digitalocean/digitalocean"
-      version = "~> 1.22.2"
-    }
-  }
-}
 
 locals {
   worker_count        = 1
--- versions.tf
+++ versions.tf
@@ -1,3 +1,17 @@
 terraform {
   required_version = ">= 0.13"
+  required_providers {
+    packet = {
+      source  = "packethost/packet"
+      version = "3.0.1"
+    }
+    ct = {
+      source  = "poseidon/ct"
+      version = "~> 0.6.1"
+    }
+    digitalocean = {
+      source  = "digitalocean/digitalocean"
+      version = "~> 1.22.2"
+    }
+  }
 }
