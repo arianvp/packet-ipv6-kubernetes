51576e29b817055b8c6921a4154e49c730ff57d2
Arian van Putten <arian.vanputten@gmail.com>
2021-10-07 16:21:47 +0200

    Use IPXE boot

--- provision.tf
+++ provision.tf
@@ -24,29 +24,28 @@ locals {
 
   metal_asn = 65530 # NOTE: this wasn't actually documented anywhere? I found it "somewhere"
 
-  K8S_VERSION  = "v1.19.0"
+  K8S_VERSION  = "v1.22.2"
   KUBEADM_URL  = "https://storage.googleapis.com/kubernetes-release/release/${local.K8S_VERSION}/bin/linux/amd64/kubeadm"
-  KUBEADM_HASH = "f1f993f3a5a440c18474d30a5b89ac01c6ce13600d93be1f234b86289a138dab1e20496de3d4882d92b54625a2efd24b56e6b0fb40ab0b660553c06c616f08ab"
 
   KUBELET_URL  = "https://storage.googleapis.com/kubernetes-release/release/${local.K8S_VERSION}/bin/linux/amd64/kubelet"
-  KUBELET_HASH = "dd5cc49c2f867a73658e3196f147df2336d50118ff2a09690a58ea9f5f8ed57e6bee2e43407dc10afc362a8da7932fee08f380d0c787137750fe2293064cc601"
 
   KUBECTL_URL  = "https://storage.googleapis.com/kubernetes-release/release/${local.K8S_VERSION}/bin/linux/amd64/kubectl"
-  KUBECTL_HASH = "e27b8d65b49296be4366b828ef179416e50f22029d6a927e5b111ff001f553ce4603426121c1502575ee19613ff14203989b5cd42998684b66a053ae32e570ee"
 
   CALICOCTL_URL  = "https://github.com/projectcalico/calicoctl/releases/download/v3.16.0/calicoctl"
-  CALICOCTL_HASH = "c011c4b81c29e17e570850c6a4a07062a5dd7ddccfbe902994dd5bff712f3148b8e028c1c788dc738d0ed73524f635cb516a781fc22522838c8651d904dd847f"
+
+  # TODO: Change to HTTPS once ISRG X1 is used. Sent a request for that to flatcar folks. Other option is to reverse proxy flatcar
+  # re: https://community.letsencrypt.org/t/production-chain-changes/150739/1
+  # re: https://github.com/ipxe/ipxe/pull/116 re:
+  # https://github.com/ipxe/ipxe/pull/112 re:
+  # https://lists.ipxe.org/pipermail/ipxe-devel/2020-May/007042.html
+  IPXE_URL = "http://stable.release.flatcar-linux.net/amd64-usr/2905.2.5/flatcar_production_packet.ipxe"
 
 
   ignition_base = templatefile("./ignition/base.yaml", {
     KUBEADM_URL    = local.KUBEADM_URL
-    KUBEADM_HASH   = local.KUBEADM_HASH
     KUBELET_URL    = local.KUBELET_URL
-    KUBELET_HASH   = local.KUBELET_HASH
     KUBECTL_URL    = local.KUBECTL_URL
-    KUBECTL_HASH   = local.KUBECTL_HASH
     CALICOCTL_URL  = local.CALICOCTL_URL
-    CALICOCTL_HASH = local.CALICOCTL_HASH
     metal_asn     = local.metal_asn
   })
 
@@ -71,7 +70,8 @@ resource "metal_device" "controlplane" {
   hostname         = "controlplane"
   plan             = "t1.small.x86"
   facilities       = ["ams1"]
-  operating_system = "flatcar_alpha"
+  operating_system = "custom_ipxe"
+  ipxe_script_url  = local.IPXE_URL
   billing_cycle    = "hourly"
   project_id       = data.metal_project.kubernetes.id
   user_data        = data.ct_config.controlplane.rendered
@@ -98,11 +98,12 @@ resource "metal_bgp_session" "controlplane" {
 }
 
 resource "metal_device" "worker" {
-  count            = local.worker_count
+  count            = 2
   hostname         = "worker${count.index}"
   plan             = "t1.small.x86"
   facilities       = ["ams1"]
-  operating_system = "flatcar_alpha"
+  operating_system = "custom_ipxe"
+  ipxe_script_url  = local.IPXE_URL
   billing_cycle    = "hourly"
   project_id       = data.metal_project.kubernetes.id
   user_data        = data.ct_config.worker.rendered
