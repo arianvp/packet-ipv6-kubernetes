b91e4b4713c3601d06da4ac7c2aa780c651f2658
Arian van Putten <arian.vanputten@gmail.com>
2021-10-08 11:23:36 +0200

    Use calico apiserver

--- default.nix
+++ default.nix
@@ -26,21 +26,8 @@ let
       install -Dm0755 kubeadm -t $out/bin
     '';
   };
-  calicoctl = pkgs.stdenv.mkDerivation {
-    name = "calicoctl";
-    src = pkgs.fetchurl {
-      url = "https://github.com/projectcalico/calicoctl/releases/download/v3.16.0/calicoctl";
-      sha256 = "14qv4wv8ndz9g7v2bq8w1fgzw14dfhn5byzp5jqp6q26z9vp0qx7";
-    };
-    dontUnpack = true;
-    dontBuild = true;
-    installPhase = ''
-      cp $src calicoctl
-      install -Dm0755 calicoctl -t $out/bin
-    '';
-  };
 in
 pkgs.mkShell {
   name = "packet-kubernetes";
-  buildInputs = [ pkgs.terraform_0_13 kubectl kubeadm calicoctl ];
+  buildInputs = [ pkgs.terraform_0_13 kubectl kubeadm ];
 }
