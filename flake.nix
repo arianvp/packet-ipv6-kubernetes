f70f316e0a9c392fa7c9b64627f51564ae3cbea8
Arian van Putten <arian.vanputten@gmail.com>
2021-10-09 11:57:40 +0200

    Add nix flakes

---
+++ flake.nix
@@ -0,0 +1,21 @@
+{
+  description = "EKS Experiment";
+
+  inputs.utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, utils, nixpkgs }:
+    utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system}; in
+      {
+        devShell = with pkgs; mkShell {
+          nativeBuildInputs = [
+            bashInteractive
+            mkcert
+            kubectl
+            kubelogin-oidc
+            terraform
+          ];
+        };
+      });
+}
+
